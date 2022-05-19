2f9d2da804faa2d566220a0788fa8c0e079115a6
wajeeh-12 <wajeeha.talal@clio.com>
2022-05-19 15:58:28 +0100

    adding Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "application#hello"
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_scope :user do
+    get 'users/sign_in', to: 'users/sessions#new', as: :new_user_session
+    get 'users/sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
+  end
+  root to: 'dashboards#show'
+  #root "application#hello"
 end
